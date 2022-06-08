57552427f4c434f27ec8e83229b5329233079df1
hasyahalim <107098931+hasyahalim@users.noreply.github.com>
2022-06-08 18:57:09 +0800

    Update project.py

--- project.py
+++ project.py
@@ -39,8 +39,6 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(["setosa", "versicolor", "virginica"])
-
 st.dataframe(["setosa", "versicolor", "virginica"])
 
 st.subheader('Prediction')
